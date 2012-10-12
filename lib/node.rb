2f2bb8ae4c420c2341b07f6c0bc10631dc7e9886
Aaron Cronin <cronin@live.co.uk>
2012-10-12 19:56:00 +0100

    misc fixes

--- lib/node.rb
+++ lib/node.rb
@@ -4,8 +4,9 @@ class Node
 
   def initialize(name, addresses)
     @name = name
-    @routing_table = Hash.new
-    (@addresses = Array(addresses)).each { |local| @routing_table[local] = ['local',0] }
+    @route_table = Hash.new
+    @addresses = Array(addresses)
+    @addresses.each { |local| @route_table[local] = { :link => 'local', :cost => 0 } }
     @links = []
   end
 
@@ -20,26 +21,37 @@ class Node
   def broadcast_table # Messages to links broadcast in a random order
     @links.shuffle.each do |link|
       target = Node.find_by_name(link)
-      puts "send #{@name} #{target.name} #{parens_table(@routing_table)}"
-      target.send(:receive_routing_table, *[@name, @routing_table])
+      puts "send #{@name} #{target.name} #{parens_table(@route_table)}"
+      target.send(:receive_route_table, *[@name, @route_table])
     end
   end
 
   def parens_table(table_hash) # Routing table in (addr|link|cost) format sorted with ascending addr.
-    table_hash.sort { |a, b| a[0] <=> b[0] }.map { |k, v| "(#{k}|#{v.first}|#{v.last})" }.join(" ")
+    table_hash.sort { |x, y| x[0] <=> y[0] }.map { |k, v| "(#{k}|#{v[:link]}|#{v[:cost]})" }.join(" ")
   end
 
   def show_table
-    puts "table #{@name} #{parens_table(@routing_table)}"
+    puts "table #{@name} #{parens_table(@route_table)}"
   end
 
-  def receive_routing_table(sender, table)
+  def receive_route_table(sender, table)
     puts "receive #{@name} #{sender} #{parens_table(table)}"
-    update_row = lambda { |k, v| (@routing_table[k] = [sender, v.last + 1]) if (@routing_table[k].nil?) || # "If there is a new destination"
-                          ((v.last + 1) < @routing_table[k].last) || # "If there is a lower cost route to an existing node"
-                          (@routing_table[k].first == sender && @routing_table[k].last != (v.last + 1))# "If recieved from N, [and N is the link]"
-    }
-    broadcast_table if table.map(&update_row).any?
+
+    changes = table.map do |target, route|
+      new_cost = route[:cost] + 1
+      should_update = @route_table[target].nil? # New destination not previously in routing table
+      should_update ||= new_cost < @route_table[target][:cost] # Lower cost link found for existing route
+      # The link responsible for an existing route has updated information
+      should_update ||= @route_table[target][:link] == sender && new_cost != @route_table[target][:cost]
+
+      if should_update
+        @route_table[target] = { :link => sender, :cost => new_cost }
+      else
+        false
+      end
+    end
+
+    broadcast_table if changes.any?
   end
 
   def self.find_by_name(name)
